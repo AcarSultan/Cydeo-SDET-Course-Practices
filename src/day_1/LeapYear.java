abed578d05557393d978efd60d5a19b61f9ffe29
Lordana <sultanacar11@gmail.com>
2024-12-14 02:02:30 +0300

    Leap year task solution

---
+++ src/day_1/LeapYear.java
@@ -0,0 +1,21 @@
+package day_1;
+
+public class LeapYear {
+    public static void main(String[] args) {
+
+        /*
+        Leap year or not
+        a leap year is defined as a year that is divisible by 4.
+        except for years that are divisible by 100.
+        However, years that are divisible by 400 are considered leap years.
+         */
+
+
+        int year= 2024;
+
+        boolean isLeapYear = (year%4==0 && year%100 !=0) || (year%400==0);
+
+        System.out.println("Is " + year + " a leap year? " + isLeapYear);
+
+    }
+}
