b368928b594dae5cd0a63580c36f08370e8b3a2e
Lordana <sultanacar11@gmail.com>
2024-12-14 18:30:19 +0300

    Kilos to pound task solution

---
+++ src/tasks/day_3/KilosToPounds.java
@@ -0,0 +1,13 @@
+package tasks.day_3;
+
+public class KilosToPounds {
+    public static void main(String[] args) {
+
+        double kg = 10.5;
+        double lb = 2.2 * kg;
+
+        System.out.println(kg + " kilos is equal to " + lb + " pounds");
+
+
+    }
+}
